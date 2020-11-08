8d4e99776fb2b8480dd97302db438d4823b06f98
Asaf <asafravona@gmail.com>
2020-11-08 10:04:08 +0200

    added basic functionality

--- momentum/src/App.js
+++ momentum/src/App.js
@@ -1,24 +1,100 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { Streak } from "./components/Streak/Streak";
+import { StreakForm } from "./components/StreakForm/StreakForm";
 
 function App() {
+  const [streaks, setStreaks] = useState([
+    {
+      title: "Stop Smoking",
+      motivation: "So I can play soccer at the park with my kids",
+      isActive: false,
+    },
+    {
+      title: "Jog every 2 days for a year",
+      motivation: "I want to lose weight and get in shape",
+      isActive: false,
+    },
+    {
+      title: "Learn 10 words in Spanish every day",
+      motivation:
+        "I want to speak with the local during my upcoming trip to Mexico",
+      isActive: false,
+    },
+  ]);
+
+  const [title, setTitle] = useState("");
+  const [motivation, setMotivation] = useState("");
+
+  const createNewStreak = (e) => {
+    const newStreaks = [...streaks];
+    newStreaks.push({
+      title: `${title}`,
+      motivation: `${motivation}`,
+      isActive: false,
+    });
+    setStreaks(newStreaks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.StrictMode>
+        <div className="streaks">
+          {streaks.map((streak, i) => (
+            <Streak
+              id={i}
+              key={i}
+              title={streak.title}
+              motivation={streak.motivation}
+              status={streak.isActive}
+            />
+          ))}
+        </div>
+
+        <hr></hr>
+
+        <form id="StreakForm" className="StreakForm">
+          <div className="StreakForm_section">
+            <label className="StreakForm_label">Name your streak:</label>
+            <input
+              type="text"
+              name="title"
+              value={title}
+              placeholder="Ex: Stopped smoking"
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="StreakForm_section">
+            <label className="StreakForm_label">
+              Why maintaining this streak is important to you?
+            </label>
+            <input
+              type="text"
+              name="motivation"
+              value={motivation}
+              placeholder="Ex: Stopped smoking"
+              onChange={(e) => setMotivation(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="StreakForm_section">
+            <button
+              value="Create"
+              onClick={(e) => {
+                e.preventDefault();
+                createNewStreak(e);
+              }}
+            >
+              Create
+            </button>
+          </div>
+        </form>
+
+        <hr></hr>
+      </React.StrictMode>
     </div>
   );
 }
